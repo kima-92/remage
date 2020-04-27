4494c34b4451d46796f565dbe08c1de165d7f8a1
Wilma <kima_92@fastmail.com>
2020-04-27 10:28:03 -0400

    Created the ReminderRepresentation Model

---
+++ Remage/Models/Reminder+Convenience.swift
@@ -0,0 +1,13 @@
+//
+//  Reminder+Convenience.swift
+//  Remage
+//
+//  Created by macbook on 4/23/20.
+//  Copyright © 2020 WilmaRodz. All rights reserved.
+//
+
+import Foundation
+
+extension Reminder {
+    
+}
