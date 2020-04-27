be0603734b1856ba6de3f3cc9d4b56b48b6ed242
Wilma <kima_92@fastmail.com>
2020-04-27 10:48:31 -0400

    Made all of the ReminderRepresentation's properties optional so we can initialize it using only an id

--- Remage/Models/ReminderRepresentation.swift
+++ Remage/Models/ReminderRepresentation.swift
@@ -11,19 +11,22 @@ import Foundation
 struct ReminderRepresentation: Codable {
     
     var id: String
-    var title: String
-    var note: String
-    var priority: String
+    var title: String?
+    var note: String?
+    var priority: String?
     
-    var defaultImage: Data
-    var image1: Data
-    var image2: Data
-    var image3: Data
-    var image4: Data
-    var image5: Data
+    var defaultImage: Data?
+    // TODO: - The default image shouldn't be optional
+    // Either save a default image or make a thumbnail out of the note or title and use that as the default image
     
-    var dayCreated: Date
-    var dueDate: Date
+    var image1: Data?
+    var image2: Data?
+    var image3: Data?
+    var image4: Data?
+    var image5: Data?
+    
+    var dayCreated: Date?
+    var dueDate: Date?
     
     init(id: String) {
         
