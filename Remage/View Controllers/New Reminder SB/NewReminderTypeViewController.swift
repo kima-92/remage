e2c43f58454388ac7d45585a18676004161e25b9
Wilma <kima_92@fastmail.com>
2020-04-27 13:04:34 -0400

    Created class files for the NewReminderTypeVC, NewReminderDetailVC and CameraVC. Wired them to UI

---
+++ Remage/View Controllers/New Reminder SB/NewReminderTypeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewReminderTypeViewController.swift
+//  Remage
+//
+//  Created by macbook on 4/27/20.
+//  Copyright © 2020 WilmaRodz. All rights reserved.
+//
+
+import UIKit
+
+class NewReminderTypeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
