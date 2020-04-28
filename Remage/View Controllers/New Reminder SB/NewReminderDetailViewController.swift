9306d286fd87b8dc59c1581b97a273d80f90a9e8
Wilma <kima_92@fastmail.com>
2020-04-28 10:46:24 -0400

    Wired up NewReminderDetailVC to UI

--- Remage/View Controllers/New Reminder SB/NewReminderDetailViewController.swift
+++ Remage/View Controllers/New Reminder SB/NewReminderDetailViewController.swift
@@ -12,12 +12,32 @@ class NewReminderDetailViewController: UIViewController {
     
     // MARK: - Properties
     var cdModelController: CoreDataModelController?
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var noteTextView: UITextView!
+    
+    @IBOutlet weak var addImagesButton: UIButton!
+    @IBOutlet weak var setAlertButton: UIButton!
+    @IBOutlet weak var setDueDateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
+    // MARK: - Actions
+    @IBAction func addImagesButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func setAlertButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func setDueDateButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
     /*
     // MARK: - Navigation
 
