12347680f60aae3874619f1cc802baec15bd8ca7
Wilma <kima_92@fastmail.com>
2020-04-29 14:21:51 -0400

    Created a class file for the Reminder Cell of the remindersCollectionView in the RemindersVC

---
+++ Remage/Views/ReminderCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderCollectionViewCell.swift
+//  Remage
+//
+//  Created by macbook on 4/29/20.
+//  Copyright © 2020 WilmaRodz. All rights reserved.
+//
+
+import UIKit
+
+class ReminderCollectionViewCell: UICollectionViewCell {
+    
+}
