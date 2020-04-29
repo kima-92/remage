a57af1228924915d43c2e2877d07d5b25b8800da
Wilma <kima_92@fastmail.com>
2020-04-29 14:40:51 -0400

    Adjusted size of the individual Cells so 3 can fit in one row

--- Remage/View Controllers/Reminders SB/RemindersViewController.swift
+++ Remage/View Controllers/Reminders SB/RemindersViewController.swift
@@ -10,35 +10,35 @@ import UIKit
 
 class RemindersViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
-    
+    // MARK: - Outlets
     @IBOutlet weak var remindersCollectionView: UICollectionView!
     
+    // MARK: - ViewDidload
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateViews()
+    }
     
+    // MARK: - Methods
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 10
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let testCell = collectionView.dequeueReusableCell(withReuseIdentifier: "testCell", for: indexPath)
+        let testCell = collectionView.dequeueReusableCell(withReuseIdentifier: "reminderCell", for: indexPath)
         testCell.backgroundColor = .cyan
         
         return testCell
     }
     
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        updateViews()
-    }
-    
+    // Update Views when ViewDidLoad is called
     func updateViews() {
         
         // Set up the remindersCollectionView's delegate and data source
         remindersCollectionView.delegate = self
         remindersCollectionView.dataSource = self
     }
-    
 
     /*
     // MARK: - Navigation
@@ -49,5 +49,4 @@ class RemindersViewController: UIViewController, UICollectionViewDataSource, UIC
         // Pass the selected object to the new view controller.
     }
     */
-
 }
