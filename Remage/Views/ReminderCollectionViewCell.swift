2c507a2e01cc03c2a4b0ab09e778b03fbecd7ecf
Wilma <kima_92@fastmail.com>
2020-04-29 15:27:48 -0400

    Added a shadow under each cell

--- Remage/Views/ReminderCollectionViewCell.swift
+++ Remage/Views/ReminderCollectionViewCell.swift
@@ -10,4 +10,29 @@ import UIKit
 
 class ReminderCollectionViewCell: UICollectionViewCell {
     
+//    var reminder: Reminder? {
+//        didSet {
+//            updateViews()
+//        }
+//    }
+    
+    override func awakeFromNib() {
+        updateViews()
+    }
+    
+    func updateViews() {
+        self.contentView.layer.cornerRadius = 2.0
+        self.contentView.layer.borderWidth = 1.0
+        self.contentView.layer.borderColor = UIColor.clear.cgColor
+        self.contentView.layer.masksToBounds = true
+        
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        self.layer.shadowRadius = 2.0
+        self.layer.shadowOpacity = 0.5
+        self.layer.masksToBounds = false
+        
+        self.layer.shadowPath = UIBezierPath(roundedRect: self.bounds, cornerRadius: self.contentView.layer.cornerRadius).cgPath
+    }
+    
 }
