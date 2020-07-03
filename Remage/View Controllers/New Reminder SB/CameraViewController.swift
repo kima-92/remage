f607bcb2a92a258f800ab5fca98c2dc0811632a6
Wilma <kima_92@fastmail.com>
2020-07-03 10:44:28 -0400

    Created CameraController. Preview Layer is displayed on screen

--- Remage/View Controllers/New Reminder SB/CameraViewController.swift
+++ Remage/View Controllers/New Reminder SB/CameraViewController.swift
@@ -12,28 +12,42 @@ import AVFoundation
 class CameraViewController: UIViewController {
     
     // MARK: - Properties
-    var cdModelController: CoreDataModelController?
     
-    var captureSession: AVCaptureSession?
-    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
-    var frontCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)
-    var backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+    var cdModelController: CoreDataModelController?
+    var cameraController: CameraController?
+    
+    var captureType: CaptureType = .picture
+    var selectedCamera: SelectedCamera = .back
+    
+    var photoOutput: AVCapturePhotoOutput?
+    var videoOutput: AVCaptureMovieFileOutput?
+    
+    var image: UIImage?
     
     // MARK: - Outlets
-    @IBOutlet weak var cameraView: UIView!
+    
+    @IBOutlet weak var cameraView: CameraPreviewView!
     
     @IBOutlet weak var captureImageButton: UIButton!
     @IBOutlet weak var rotateCameraButton: UIButton!
     @IBOutlet weak var flashButton: UIButton!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        checkIOSVersion()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super .viewDidAppear(animated)
+        setupSession()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        stopSession()
     }
     
     // MARK: - Actions
+    
     @IBAction func captureImageButtonTapped(_ sender: UIButton) {
     }
     @IBAction func rotateCameraButtonTapped(_ sender: UIButton) {
@@ -42,30 +56,38 @@ class CameraViewController: UIViewController {
     }
     
     // MARK: - Methods
-    func checkIOSVersion() {
-        if #available(iOS 10.2, *) {
-            let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
-            
-            do {
-                let input = try AVCaptureDeviceInput(device: captureDevice!)
-                captureSession = AVCaptureSession()
-                captureSession?.addInput(input)
-                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
-                videoPreviewLayer?.frame = view.layer.bounds
-                captureSession?.startRunning()
-            } catch {
-                NSLog("Could not access Camera due to the iOS Version of the device")
-            }
-        }
+    
+    // Setup CaptureSession
+    private func setupSession() {
+        guard let cameraController = cameraController else { return }
+        
+        cameraController.cameraPreviewView = cameraView
+        cameraController.captureType = captureType
+        cameraController.selectedCamera = selectedCamera
+        
+        cameraController.setupCaptureSession()
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Stop Running CaptureSession
+    private func stopSession() {
+        guard let cameraController = cameraController else { return }
+        cameraController.stopRunningSession()
     }
-    */
+}
+
+// MARK: - Extensions
+
+extension CameraViewController: AVCapturePhotoCaptureDelegate {
+    
+    // To get information about the captured image, with the data captured from output
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        
+        // This method gives you the still image in data, in the parameter "photo"
+        // Convert it to UIImage
+        if let imageData = photo.fileDataRepresentation() {
+            image = UIImage(data: imageData)
+            
+            performSegue(withIdentifier: "ShowPhotoPreviewSegue", sender: self)
+        }
+    }
 }
