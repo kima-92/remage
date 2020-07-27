111304d912750e7de15a0aa4eb040b96e098cf30
Wilma <kima_92@fastmail.com>
2020-07-27 17:54:24 -0400

    Created the SettingsMenuVC and wired it up to Storyboard

---
+++ Remage/View Controllers/Settings SB/SettingsMenuViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsMenuViewController.swift
+//  Remage
+//
+//  Created by macbook on 7/27/20.
+//  Copyright © 2020 WilmaRodz. All rights reserved.
+//
+
+import UIKit
+
+class SettingsMenuViewController: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var backgroundCardView: UIView!
+    @IBOutlet weak var backgroundLabel: UILabel!
+    
+    // MARK: - DidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
