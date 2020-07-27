97bac0974aedfdc5c912ccad8f217c1137ea41f2
Wilma <kima_92@fastmail.com>
2020-07-27 16:36:34 -0400

    Added Alert to NRNoteVC, alerting the User if the Note wasn't able to be saved

--- Remage/View Controllers/New Reminder SB/NRNoteViewController.swift
+++ Remage/View Controllers/New Reminder SB/NRNoteViewController.swift
@@ -45,20 +45,35 @@ class NRNoteViewController: UIViewController {
         noteTextView.text = note
     }
     
+    // Send Note via GetNoteDelegate Protocol
     private func sendNoteToReminder() {
         
-        guard let note = noteTextView.text,
-                !note.isEmpty
-            else {
-                // TODO: - Alert User
-                return
+        // Try to send Note
+        if let note = noteTextView.text,
+            !note.isEmpty {
+            
+            getNoteDelegate.get(note: note)
+            
+        // Else alert user
+        } else {
+            showCantSaveNote()
+            return
         }
-        getNoteDelegate.get(note: note)
+    }
+    
+    // Can't save Note Alert
+    private func showCantSaveNote() {
+        let alert = UIAlertController(title: "Oops!", message: "Something when't wrong, couldn't save this Note. Please try again.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        self.present(alert, animated: true)
     }
     
     // MARK: - UpdateViews
     
     private func updateViews() {
+        
         // Round corners
         backgroundCardView.layer.cornerRadius = 15
         noteTextView.layer.cornerRadius = 15
