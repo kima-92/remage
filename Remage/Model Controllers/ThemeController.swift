1c91986654cadd8e6fe8391a066e29c1a4a33983
Wilma <kima_92@fastmail.com>
2020-07-28 19:30:05 -0400

    BGColor Model, ColorList Class and ThemeController class. Added the first two background colors for the App

---
+++ Remage/Model Controllers/ThemeController.swift
@@ -0,0 +1,21 @@
+//
+//  ThemeController.swift
+//  Remage
+//
+//  Created by macbook on 7/28/20.
+//  Copyright © 2020 WilmaRodz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ThemeController {
+    
+    // MARK: - Properties
+    
+    let colorList = ColorList()
+    
+    var bgColors: [BGColor] {
+        return colorList.bgColors
+    }
+}
