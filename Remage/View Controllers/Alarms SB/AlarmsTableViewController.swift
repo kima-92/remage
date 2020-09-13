1c45730489361525b9bd30e930707ec1c9709d58
Wilma <kima_92@fastmail.com>
2020-09-13 16:46:54 -0400

    Code cleanup

--- Remage/View Controllers/Alarms SB/AlarmsTableViewController.swift
+++ Remage/View Controllers/Alarms SB/AlarmsTableViewController.swift
@@ -100,21 +100,6 @@ class AlarmsTableViewController: UITableViewController, NSFetchedResultsControll
         }    
     }
     */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
     
     // MARK: - Methods
     
